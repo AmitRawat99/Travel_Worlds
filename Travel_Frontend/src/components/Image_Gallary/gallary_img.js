380a0be9e54791a3d250bf16d301d304fac15082
Amit Rawat <167463184+AmitRawat99@users.noreply.github.com>
2025-06-04 14:25:42 +0530

    Update gallary_img.js

--- Travel_Frontend/src/components/Image_Gallary/gallary_img.js
+++ Travel_Frontend/src/components/Image_Gallary/gallary_img.js
@@ -1,11 +1,11 @@
-import galleryImg01 from '../../../public/images/gallery-01.jpg'
-import galleryImg02 from '../../../public/images/gallery-02.jpg'
-import galleryImg03 from '../../../public/images/gallery-03.jpg'
-import galleryImg04 from '../../../public/images/gallery-04.jpg'
-import galleryImg05 from '../../../public/images/gallery-05.jpg'
-import galleryImg06 from '../../../public/images/gallery-06.jpg'
-import galleryImg07 from '../../../public/images/gallery-07.jpg'
-import galleryImg08 from '../../../public/images/gallery-08.jpg'
+import galleryImg01 from ../../assets/Images/gallery-01.jpg'
+import galleryImg02 from '../../assets/Images/gallery-02.jpg'
+import galleryImg03 from '../../assets/Images/gallery-03.jpg'
+import galleryImg04 from '../../assets/Images/gallery-04.jpg'
+import galleryImg05 from '../../assets/Images/gallery-05.jpg'
+import galleryImg06 from '../../assets/Images/gallery-06.jpg'
+import galleryImg07 from '../../assets/Images/gallery-07.jpg'
+import galleryImg08 from '../../assets/Images/gallery-08.jpg'
 
 const GalleryImages = [
     galleryImg01,
