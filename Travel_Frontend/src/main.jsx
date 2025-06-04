fc8a6f2f7a4681b217db651c798350f5c4fda11e
Amit Rawat <167463184+AmitRawat99@users.noreply.github.com>
2025-06-04 14:16:56 +0530

    Update main.jsx

--- Travel_Frontend/src/main.jsx
+++ Travel_Frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'remixicon/fonts/remixicon.css'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthContextProvider } from './context/authContext'
+import { AuthContextProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
